fcdce70fb7c700531e6a5baf28d934e57dea6945
Chris Ollenburg <chris.ollenburg@gmail.com>
2019-12-14 10:02:15 -0700

    IntComputer Updates Added multiple input support. Some minor refactoring and renaming to make things more readable.

--- src/main/kotlin/ollendev/aoc/IntComputer.kt
+++ src/main/kotlin/ollendev/aoc/IntComputer.kt
@@ -1,30 +1,34 @@
 package ollendev.aoc
 
 fun main() {
-//    fixComputer()
-//    findPossibleCombinations()
-//    runTESTDiagnostic()
+    println("Fix Computer\n-------------")
+    fixComputer()
+    println("\nFind Combination\n-------------")
+    findPossibleCombinations()
+    println("\nRun TEST Diag\n-------------")
+    runTESTDiagnostic()
+    println("\nRun Radiator Diag\n-------------")
     runRadiatorTestDiagnostic()
 }
 
 // Day 5 Part 1 -  Thermal Environment Supervision Terminal (TEST)
 fun runTESTDiagnostic() {
     val ram = initRam("TESTDiagnosticProgram.txt")
-    val computer = IntComputer(ram, 1)
+    val computer = IntComputer(ram, mutableListOf(1))
     computer.run()
 }
 
 // Day 5 Part 2 -  Thermal Radiator
 fun runRadiatorTestDiagnostic() {
     val ram = initRam("TESTDiagnosticProgram.txt")
-    val computer = IntComputer(ram, 5)
+    val computer = IntComputer(ram, mutableListOf(5))
     computer.run()
 }
 
 // Day 1 Part 1 "1202 error"
 fun fixComputer() {
     val result = day1Driver(12, 2)
-    print("$result")
+    println("$result")
 }
 
 // Day 2 Part 2
@@ -53,7 +57,7 @@ fun day1Driver(noun: Int, verb: Int): Int {
     // replace position 2 with the value 2
     ram[2] = verb
 
-    val computer = IntComputer(ram, 1)
+    val computer = IntComputer(ram, mutableListOf(1))
     computer.run()
 
     return ram[0]
@@ -62,7 +66,7 @@ fun day1Driver(noun: Int, verb: Int): Int {
 fun initRam(programFile: String): MutableList<Int> {
     val ram = mutableListOf<Int>()
 
-    FileLoader.getFile("$programFile").readLines().forEach { line ->
+    FileLoader.getFile(programFile).readLines().forEach { line ->
         line.split(",").forEach { s ->
             ram.add(s.toInt())
         }
@@ -73,7 +77,7 @@ fun initRam(programFile: String): MutableList<Int> {
 
 class IntComputer(
     private val ram: MutableList<Int>,
-    private val input: Int
+    private val input: MutableList<Int>
 ) {
 
     private var programPointer = 0
@@ -81,127 +85,124 @@ class IntComputer(
     fun run() {
         if (ram.size < 4) return
 
-        var code = getCode(programPointer)
+        var instruction = getInstruction(programPointer)
 
-        runloop@ while (code.instruction != Instruction.Halt) {
+        runloop@ while (instruction.opCode != OpCode.Halt) {
             var jumpPointer: Int? = null
-            when (code.instruction) {
-                Instruction.Add -> add(code)
-                Instruction.Multiply -> multiply(code)
-                Instruction.WriteInput -> writeInput()
-                Instruction.PrintOutput -> printOutput(code)
-                Instruction.JumpIfTrue -> jumpPointer = jumpIfTrue(code)
-                Instruction.JumpIfFalse -> jumpPointer = jumpIfFalse(code)
-                Instruction.LessThan -> lessThan(code)
-                Instruction.Equals -> equals(code)
-                Instruction.Halt -> break@runloop
+            when (instruction.opCode) {
+                OpCode.Add -> add(instruction)
+                OpCode.Multiply -> multiply(instruction)
+                OpCode.Input -> input()
+                OpCode.Output -> output(instruction)
+                OpCode.JumpIfTrue -> jumpPointer = jumpIfTrue(instruction)
+                OpCode.JumpIfFalse -> jumpPointer = jumpIfFalse(instruction)
+                OpCode.LessThan -> lessThan(instruction)
+                OpCode.Equals -> isEqual(instruction)
+                OpCode.Halt -> break@runloop
             }
-            if (jumpPointer != null) {
-                programPointer = jumpPointer
-            } else {
-                programPointer += code.instruction.size
-            }
-            code = getCode(programPointer)
+            programPointer = jumpPointer ?: programPointer + instruction.opCode.size
+            instruction = getInstruction(programPointer)
         }
     }
 
-    private fun add(code: Code) {
-        val left = readParamValue(code, 0)
-        val right = readParamValue(code, 1)
+    private fun add(instruction: Instruction) {
+        val left = readParamValue(instruction, 0)
+        val right = readParamValue(instruction, 1)
         val saveAddress = ram[programPointer + 3]
         ram[saveAddress] = left + right
     }
 
-    private fun multiply(code: Code) {
-        val left = readParamValue(code, 0)
-        val right = readParamValue(code, 1)
+    private fun multiply(instruction: Instruction) {
+        val left = readParamValue(instruction, 0)
+        val right = readParamValue(instruction, 1)
         val saveAddress = ram[programPointer + 3]
         ram[saveAddress] = left * right
     }
 
-    private fun writeInput() {
+    private fun input() {
         val saveAddress = ram[programPointer + 1]
-        ram[saveAddress] = input
+        ram[saveAddress] = input.first()
+        input.removeAt(0)
     }
 
-    private fun printOutput(code: Code) {
-        val output = readParamValue(code, 0)
+    private fun output(instruction: Instruction) {
+        val output = readParamValue(instruction, 0)
         println("$output")
     }
 
     // if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter.
     // Otherwise, it does nothing.
-    private fun jumpIfTrue(code: Code): Int? {
-        val value = readParamValue(code, 0)
+    private fun jumpIfTrue(instruction: Instruction): Int? {
+        val value = readParamValue(instruction, 0)
         if (value != 0) {
-            return readParamValue(code, 1)
+            return readParamValue(instruction, 1)
         }
         return null
     }
 
     // if the first parameter is zero, it sets the instruction pointer to the value from the second parameter.
     // Otherwise, it does nothing
-    private fun jumpIfFalse(code: Code): Int? {
-        val value = readParamValue(code, 0)
+    private fun jumpIfFalse(instruction: Instruction): Int? {
+        val value = readParamValue(instruction, 0)
         if (value == 0) {
-            return readParamValue(code, 1)
+            return readParamValue(instruction, 1)
         }
         return null
     }
 
     // if the first parameter is less than the second parameter, it stores 1 in the position given by the third
     // parameter. Otherwise, it stores 0.
-    private fun lessThan(code: Code) {
+    private fun lessThan(instruction: Instruction) {
         val saveAddress = ram[programPointer + 3]
-        val left = readParamValue(code, 0)
-        val right = readParamValue(code, 1)
+        val left = readParamValue(instruction, 0)
+        val right = readParamValue(instruction, 1)
         ram[saveAddress] = if (left < right) { 1 } else { 0 }
     }
 
     // if the first parameter is equal to the second parameter, it stores 1 in the position given by the third
     // parameter. Otherwise, it stores 0.
-    private fun equals(code: Code) {
+    private fun isEqual(instruction: Instruction) {
         val saveAddress = ram[programPointer + 3]
-        val left = readParamValue(code, 0)
-        val right = readParamValue(code, 1)
+        val left = readParamValue(instruction, 0)
+        val right = readParamValue(instruction, 1)
         ram[saveAddress] = if (left == right) { 1 } else { 0 }
     }
 
-    private fun readParamValue(code: Code, paramIndex: Int): Int {
-        return if (code.getParamMode(paramIndex) == ParamMode.Immediate) {
+    private fun readParamValue(instruction: Instruction, paramIndex: Int): Int {
+        return if (instruction.getParamMode(paramIndex) == ParamMode.Immediate) {
             ram[programPointer + paramIndex + 1]
         } else {
             ram[ram[programPointer + paramIndex + 1]]
         }
     }
 
-    private fun getCode(index: Int): Code {
-        return Code(ram[index].toString())
+    private fun getInstruction(index: Int): Instruction {
+        return Instruction(ram[index].toString())
     }
 }
 
-enum class Instruction(val value: Int, val size: Int) {
-    Add(1, 4),
-    Multiply(2, 4),
-    WriteInput(3, 2),
-    PrintOutput(4, 2),
-    JumpIfTrue(5, 3),
-    JumpIfFalse(6, 3),
-    LessThan(7, 4),
-    Equals(8, 4),
-    Halt(99, 1)
+enum class OpCode(val size: Int) {
+    Add(4),
+    Multiply(4),
+    Input(2),
+    Output(2),
+    JumpIfTrue(3),
+    JumpIfFalse(3),
+    LessThan(4),
+    Equals(4),
+    Halt(1)
 }
 
-fun Int?.toInstruction() = when (this) {
-    Instruction.Add.value -> Instruction.Add
-    Instruction.Multiply.value -> Instruction.Multiply
-    Instruction.WriteInput.value -> Instruction.WriteInput
-    Instruction.PrintOutput.value -> Instruction.PrintOutput
-    Instruction.JumpIfTrue.value -> Instruction.JumpIfTrue
-    Instruction.JumpIfFalse.value -> Instruction.JumpIfFalse
-    Instruction.LessThan.value -> Instruction.LessThan
-    Instruction.Equals.value -> Instruction.Equals
-    Instruction.Halt.value -> Instruction.Halt
+fun Int?.toOpCode() = when (this) {
+    1 -> OpCode.Add
+    2 -> OpCode.Multiply
+    3 -> OpCode.Input
+    4 -> OpCode.Output
+    5 -> OpCode.JumpIfTrue
+    6 -> OpCode.JumpIfFalse
+    7 -> OpCode.LessThan
+    8 -> OpCode.Equals
+    99 -> OpCode.Halt
     else -> throw IllegalStateException("Unknown op code instruction!")
 }
 
@@ -210,12 +211,12 @@ enum class ParamMode {
     Immediate
 }
 
-data class Code(val value: String) {
-    val instruction: Instruction
+data class Instruction(val value: String) {
+    val opCode: OpCode
     private val paramModes: List<ParamMode>
 
     init {
-        instruction = parseInstruction()
+        opCode = parseOpCode()
         paramModes = parseParamModes()
     }
 
@@ -223,11 +224,11 @@ data class Code(val value: String) {
         return paramModes.getOrElse(index) { ParamMode.Address }
     }
 
-    private fun parseInstruction(): Instruction {
+    private fun parseOpCode(): OpCode {
         return if (value.length == 1) {
-            value.toInt().toInstruction()
+            value.toInt().toOpCode()
         } else {
-            value.substring(value.length-2).toInt().toInstruction()
+            value.substring(value.length-2).toInt().toOpCode()
         }
     }
 
--- src/test/kotlin/ollendev/aoc/IntComputerTest.kt
+++ src/test/kotlin/ollendev/aoc/IntComputerTest.kt
@@ -89,7 +89,7 @@ class IntComputerTest {
     @Test
     fun `Write input`() {
         val ram = mutableListOf(3,4,99,0,0)
-        runTest(ram, 55)
+        runTest(ram, mutableListOf(55))
         assertEquals(mutableListOf(3,4,99,0,55), ram)
     }
 
@@ -103,84 +103,84 @@ class IntComputerTest {
     @Test
     fun `test program input == 8 address mode`() {
         val ram = mutableListOf(3,9,8,9,10,9,4,9,99,-1,8)
-        runTest(ram, 8)
+        runTest(ram, mutableListOf(8))
         assertEquals("1\n", outContent.toString())
     }
 
     @Test
     fun `test program input != 8 address mode`() {
         val ram = mutableListOf(3,9,8,9,10,9,4,9,99,-1,8)
-        runTest(ram, 5)
+        runTest(ram, mutableListOf(5))
         assertEquals("0\n", outContent.toString())
     }
 
     @Test
     fun `test program input less than 8 address mode`() {
         val ram = mutableListOf(3,9,7,9,10,9,4,9,99,-1,8)
-        runTest(ram, 6)
+        runTest(ram, mutableListOf(6))
         assertEquals("1\n", outContent.toString())
     }
 
     @Test
     fun `test program input greater than or equal 8 address mode`() {
         val ram = mutableListOf(3,9,7,9,10,9,4,9,99,-1,8)
-        runTest(ram, 8)
+        runTest(ram, mutableListOf(8))
         assertEquals("0\n", outContent.toString())
     }
 
     @Test
     fun `test program input equal to 8 immediate mode`() {
         val ram = mutableListOf(3,3,1108,-1,8,3,4,3,99)
-        runTest(ram, 8)
+        runTest(ram, mutableListOf(8))
         assertEquals("1\n", outContent.toString())
     }
 
     @Test
     fun `test program input not equal to 8 immediate mode`() {
         val ram = mutableListOf(3,3,1108,-1,8,3,4,3,99)
-        runTest(ram, 9)
+        runTest(ram, mutableListOf(9))
         assertEquals("0\n", outContent.toString())
     }
 
     @Test
     fun `test input less than 8 immediate mode`() {
         val ram = mutableListOf(3,3,1107,-1,8,3,4,3,99)
-        runTest(ram, 5)
+        runTest(ram, mutableListOf(5))
         assertEquals("1\n", outContent.toString())
     }
 
     @Test
     fun `test input not less than 8 immediate mode`() {
         val ram = mutableListOf(3,3,1107,-1,8,3,4,3,99)
-        runTest(ram, 8)
+        runTest(ram, mutableListOf(8))
         assertEquals("0\n", outContent.toString())
     }
 
     @Test
     fun `jump test 0, output 1 - address mode`() {
         val ram = mutableListOf(3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9)
-        runTest(ram, 20)
+        runTest(ram, mutableListOf(20))
         assertEquals("1\n", outContent.toString())
     }
 
     @Test
     fun `jump test 0, output 0 - address mode`() {
         val ram = mutableListOf(3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9)
-        runTest(ram, 0)
+        runTest(ram, mutableListOf(0))
         assertEquals("0\n", outContent.toString())
     }
 
     @Test
     fun `jump test 1, output 1 - immediate mode`() {
         val ram = mutableListOf(3,3,1105,-1,9,1101,0,0,12,4,12,99,1)
-        runTest(ram, 20)
+        runTest(ram, mutableListOf(20))
         assertEquals("1\n", outContent.toString())
     }
 
     @Test
     fun `jump test 1, output 0 - immediate mode`() {
         val ram = mutableListOf(3,3,1105,-1,9,1101,0,0,12,4,12,99,1)
-        runTest(ram, 0)
+        runTest(ram, mutableListOf(0))
         assertEquals("0\n", outContent.toString())
     }
 
@@ -189,7 +189,7 @@ class IntComputerTest {
         val ram = mutableListOf(3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
             1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
             999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99)
-        runTest(ram, -1)
+        runTest(ram, mutableListOf(-1))
         assertEquals("999\n", outContent.toString())
     }
 
@@ -198,7 +198,7 @@ class IntComputerTest {
         val ram = mutableListOf(3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
             1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
             999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99)
-        runTest(ram, 8)
+        runTest(ram, mutableListOf(8))
         assertEquals("1000\n", outContent.toString())
     }
 
@@ -207,17 +207,17 @@ class IntComputerTest {
         val ram = mutableListOf(3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
             1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
             999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99)
-        runTest(ram, 200)
+        runTest(ram, mutableListOf(200))
         assertEquals("1001\n", outContent.toString())
     }
 
-    private fun runTest(ram: MutableList<Int>, input: Int = 1) {
+    private fun runTest(ram: MutableList<Int>, input: MutableList<Int> = mutableListOf(1)) {
         val computer = IntComputer(ram, input)
         computer.run()
     }
 }
 
-class CodeTest {
+class InstructionTest {
 
     @Test
     fun `01001 is an add instruction`() {
@@ -261,7 +261,7 @@ class CodeTest {
 
     @Test
     fun `11101 yields 3 immediate modes`() {
-        val code = Code("11101")
+        val code = Instruction("11101")
         assertEquals(ParamMode.Immediate, code.getParamMode(0))
         assertEquals(ParamMode.Immediate, code.getParamMode(1))
         assertEquals(ParamMode.Immediate, code.getParamMode(2))
@@ -269,7 +269,7 @@ class CodeTest {
 
     @Test
     fun `missing codes default to address mode`() {
-        val code = Code("02")
+        val code = Instruction("02")
         assertEquals(ParamMode.Address, code.getParamMode(0))
         assertEquals(ParamMode.Address, code.getParamMode(1))
         assertEquals(ParamMode.Address, code.getParamMode(2))
@@ -277,24 +277,24 @@ class CodeTest {
 
     @Test
     fun `partial codes parsed correctly`() {
-        val code = Code("1002")
+        val code = Instruction("1002")
         assertEquals(ParamMode.Address, code.getParamMode(0))
         assertEquals(ParamMode.Immediate, code.getParamMode(1))
         assertEquals(ParamMode.Address, code.getParamMode(2))
     }
 
     private fun assertAddInstruction(input: String) {
-        val code = Code(input)
-        assertEquals(Instruction.Add, code.instruction)
+        val code = Instruction(input)
+        assertEquals(OpCode.Add, code.opCode)
     }
 
     private fun assertMultiplyInstruction(input: String) {
-        val code = Code(input)
-        assertEquals(Instruction.Multiply, code.instruction)
+        val code = Instruction(input)
+        assertEquals(OpCode.Multiply, code.opCode)
     }
 
     private fun assertHaltInstruction(input: String) {
-        val code = Code(input)
-        assertEquals(Instruction.Halt, code.instruction)
+        val code = Instruction(input)
+        assertEquals(OpCode.Halt, code.opCode)
     }
 }
